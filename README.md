120db62911109907e86c2b7c87b5596869a6f56f
Connor <connorbuchko2010@yahoo.ca>
2020-04-24 22:02:51 -0700

    conditional rendering on scroll arrow

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# personal website
+## to run locally
+1. `npm install`
+2. `npm run webpack`
+3. run site on localhost
--- src/script.js
+++ src/script.js
@@ -5,11 +5,31 @@ class Scroller extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            scrollDistance: window.pageYOffset
         };
     }
+    componentDidUpdate() {
+        console.log("poop");
+    }
 
-    render(){
-        
+    render() { 
+        if(this.state.scrollDistance < 5){    
+            return (
+                <div>
+                    <a href="#about-me"><img className="down-arrow" src="./images/down-arrow.svg"></img></a>
+                </div>
+            );
+        }
+        return null;
+        }
+}
+
+function renderScroller(){
+    if(document.querySelector("#down-scroll") != null){
+        console.log("asdfasdfsd");
+        let domContainer = document.querySelector("#down-scroll");
+        ReactDOM.render(<Scroller />, domContainer);
     }
 }
+  
+  setInterval(renderScroller, 250);
